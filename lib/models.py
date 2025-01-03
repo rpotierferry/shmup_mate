e4326f8a404a311cf35960e484ccbcf4696b25e2
Romain Potier Ferry <romainpotierferry@gmail.com>
2025-01-03 17:41:38 +0100

    model typing

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,6 @@
 class Game:
     def __init__(self, game_info):
-        self.id = game_info["id"]
+        self.id = int(game_info["id"])
         self.title = game_info["title"]
         self.dev = game_info["dev"]
         self.platform = game_info["platform"]
@@ -8,17 +8,17 @@ class Game:
 
 class Run:
     def __init__(self, run_info):
-        self.id = run_info["id"]
-        self.game_id = run_info["game_id"]
+        self.id = int(run_info["id"])
+        self.game_id = int(run_info["game_id"])
         self.state = run_info["state"]
-        self.score = run_info["score"]
-        self.stage = run_info["stage"]
+        self.score = int(run_info["score"])
+        self.stage = (run_info["stage"])
         self.remarks = []
 
 class Remark:
     def __init__(self, rem_info):
-        self.id = rem_info["id"]
-        self.run_id = rem_info["run_id"]
+        self.id = int(rem_info["id"])
+        self.run_id = int(rem_info["run_id"])
         self.text = rem_info["text"]
 
 class Stages:
